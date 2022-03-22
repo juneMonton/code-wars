975469ddce1fd25c057d0175d49e6488f4e00ccc
June Monton <june.webdeveloper@gmail.com>
2022-03-22 21:18:10 +0800

    added solutions to codewars new kata

--- kata.js
+++ kata.js
@@ -52,3 +52,50 @@ function lovefunc(flower1, flower2) {
 }
 //complicated soultion:
 let lovefunc = (f1, f2) => !!((f1 + f2) % 2)
+
+/*Complete the square sum function so that it squares each number passed into it and then sums the results together.
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.*/
+
+//Solutions:
+function squareSum(numbers) {
+    return numbers.reduce(function (sum, n) {
+        return (n * n) + sum;
+    }, 0)
+}
+
+//another solution:
+function squareSum(numbers) {
+    return numbers.map(square).reduce(sum);
+}
+
+function square(number) {
+    return number * number;
+}
+
+function sum(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+
+//another solution:
+function squareSum(numbers) {
+    return numbers.reduce((sum, num) => sum + (num * num), 0);
+}
+
+/* We need a function that can transform a string into a number. What ways of achieving this do you know?
+Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.*/
+var stringToNumber = function(str){
+    return parseInt(str);
+  }
+
+ //another solution
+ var stringToNumber = function(str){
+    // put your code here
+    return Number(str);
+  }
+  
+
+  //another solution
+  const stringToNumber = str => Number(str)
+
+  //another solution
+  const stringToNumber = s => parseInt(s);
